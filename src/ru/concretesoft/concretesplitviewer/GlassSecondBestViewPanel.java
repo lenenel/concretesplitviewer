42fcc83487a2d9ae82fe9eb1150c4613dc0741c5
Leonid.Mytinski <Leonid.Mytinski@38dd0895-a92f-0410-8b9d-4fc7e21a3f08>
2007-09-05 18:53:04 +0000

    Fixed build fail
    git-svn-id: http://concretesplitviewer.googlecode.com/svn/trunk@49 38dd0895-a92f-0410-8b9d-4fc7e21a3f08

--- src/ru/concretesoft/concretesplitviewer/GlassSecondBestViewPanel.java
+++ src/ru/concretesoft/concretesplitviewer/GlassSecondBestViewPanel.java
@@ -43,16 +43,7 @@ public class GlassSecondBestViewPanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        setLayout(new java.awt.GridBagLayout());
     }// </editor-fold>//GEN-END:initComponents
 
     public void setCPsNumber(int numberOfCP) {
--- src/ru/concretesoft/concretesplitviewer/GlassStandartSplitViewerPanel.java
+++ src/ru/concretesoft/concretesplitviewer/GlassStandartSplitViewerPanel.java
@@ -37,16 +37,7 @@ public class GlassStandartSplitViewerPanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        setLayout(new java.awt.GridBagLayout());
     }// </editor-fold>//GEN-END:initComponents
     @Override
     public void paintComponent(java.awt.Graphics g){
