7bd5e6ff0d2f8a6ea81a2c9cf77d4b61ec7a64fc
Leonid.Mytinski <Leonid.Mytinski@38dd0895-a92f-0410-8b9d-4fc7e21a3f08>
2007-09-17 18:52:12 +0000

    Modified model for work with numbers of control points
    git-svn-id: http://concretesplitviewer.googlecode.com/svn/trunk@55 38dd0895-a92f-0410-8b9d-4fc7e21a3f08

--- src/ru/concretesoft/concretesplitviewer/Distance.java
+++ src/ru/concretesoft/concretesplitviewer/Distance.java
@@ -13,7 +13,7 @@ import java.util.Vector;
 
 /**
  *
- * @author Мытинский Леонид
+ * @author Mytinski Leonid
  *
  * Класс описывающий дистанцию
  */
@@ -21,25 +21,38 @@ public class Distance {
     private String name;
     private int length;
     private int numberOfCP;
-    private int[] lengths;
+//    private int[] lengths;
+    private Lap[] laps;
     private Vector<Group> groups;
     /** Creates a new instance of Distance 
      *
-     * name - название дистанции (обычно название группы)
-     * length - длина дистанции 
-     * numberOfCP - количество пунктов
-     * lengths - массив содержащий длины перегонов
+     * @param  name  название дистанции (обычно название группы)
+     * @param  length  длина дистанции 
+     * @param  numberOfCP  количество пунктов
+     * @param  lengths  массив содержащий длины перегонов
      *
      */
     public Distance(String name, int length, int numberOfCP, int[] lengths) {
+        this(name, length, numberOfCP, lengths, null);
+    }
+    public Distance(String name, int length, int numberOfCP, int[] lengths, int[] cPsNumbers){
         this.name = name;
         this.length = length;
-        this.lengths = lengths;
+//        this.lengths = lengths;
         this.numberOfCP = numberOfCP;
+        laps = new Lap[numberOfCP];
+        if((lengths!=null)&&(cPsNumbers!=null)&&(lengths.length==cPsNumbers.length)){
+            laps[0] = new Lap(Lap.START_CONTROL_POINT, cPsNumbers[0], lengths[0]);
+            for(int i = 1; i < lengths.length; i++){
+                laps[i] = new Lap(cPsNumbers[i-1], cPsNumbers[i], lengths[i]);
+            }
+        } else if(lengths!=null){
+                setLengthsOfDists(lengths);
+        }
         groups = new Vector<Group>();
     }
     public Distance(String name, int length, int numberOfCP){
-        this(name, length, numberOfCP, null);
+        this(name, length, numberOfCP, null, null);
     }
     /*
      * Метод возвращает название дистанции.
@@ -75,15 +88,18 @@ public class Distance {
       *
       */
     public int getLengthOfDist(int n){
-        if(lengths==null) return -1;
-        return lengths[n-1];
+        if(laps[n-1]==null) return -1;
+        return laps[n-1].getLength();
     }
      /*
       * Метод устанавливает длину перегона на пункт n равной l.
       */
     public void setLengthOfDist(int n, int l){
-        if(lengths==null) lengths = new int[getNumberOfCP()+1];
-        lengths[n-1] = l;
+        if(laps[n-1]==null) {
+            laps[n-1] = new Lap(l);
+        } else {
+            laps[n-1].setLength(l);
+        }
     }
     
     /** Method for set all lengths of laps.
@@ -116,12 +132,14 @@ public class Distance {
      /*
      * Метод устанавливает набор групп для этой дистанции
      */
+    @SuppressWarnings("unchecked")
     public void setGroups(Vector<Group> grs){
         groups = (Vector<Group>)grs.clone();
     }
      /*
      * Метод возвращает все группы принадлижащие этой дистанции.
      */
+    @SuppressWarnings("unchecked")
     public Vector<Group> getGroups(){
         return (Vector<Group>) groups.clone();
     }
